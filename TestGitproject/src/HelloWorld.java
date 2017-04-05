955e3023b88e90b8c34e29c9b3ee5a5b9688fe80
ramkseth <ramkseth@DIN76000160.corp.capgemini.com>
2017-04-05 19:12:03 +0530

    new branch commit

--- TestGitproject/src/HelloWorld.java
+++ TestGitproject/src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("This is hello World");
 		System.out.println("New comment");
-		System.out.print("created branch");
+		System.out.print("created branches");
 	}
 
 }
